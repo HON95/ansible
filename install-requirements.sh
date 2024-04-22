44a645e32e9ce6f23a39c3a24849db011d863e8a
full_name <email_addr>
2024-04-22 22:37:36 +0200

    Update requirements and linting

--- install-requirements.sh
+++ install-requirements.sh
@@ -4,18 +4,17 @@
 
 set -eu -o pipefail
 
-the_dir="$(dirname $0)"
+echo
+if [[ -f "/etc/arch-release" ]]; then
+    cat "requirements/arch.txt" | grep -Pv '^#|^$' | yay -S --needed --noconfirm -
+else
+    python3 -m pip install -U -r "requirements/python.txt"
+fi
 
 echo
-python3 -m pip install -U -r "$the_dir/requirements-python.txt"
+ansible-galaxy collection install --force -U -r "requirements/ansible.yml"
+echo
+ansible-galaxy role install --force -r "requirements/ansible.yml"
 
 echo
-ansible-galaxy collection install -U -r "$the_dir/requirements-ansible.yml"
-
-# TODO: Remove this and update ansible-reqs once lae.proxmox supports Deb12+PVE8.
-# https://github.com/lae/ansible-role-proxmox/pull/230
-echo
-ansible-galaxy install https://github.com/lexxxel/ansible-role-proxmox/archive/refs/heads/feature/add_bookworm_and_debian_12_compatibility.tar.gz,pr-230,lae.proxmox --force
-
-echo
-echo "Done!"
+echo -e "\033[0;32m""Done!""\033[0m"
--- lint.sh
+++ lint.sh
@@ -8,4 +8,5 @@ local_dir="$(dirname $0)"
 
 ansible-lint -c "$local_dir/.ansible-lint" "$local_dir/roles/"*
 
-echo "OK!"
+echo
+echo -e "\033[0;32m""OK!""\033[0m"
